add2b281ce872639ed71494f23dab8c489301970
sam <samuel.j.simmons@gmail.com>
2016-04-28 13:21:31 +1200

    set up webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import Post from './Post'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <Post/>
+    )
+  }
+}
+
+export default App
