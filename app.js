7cb67e0d667aea12ba2a5f01cacc32a1e052acdc
Vicken <vicken.liu@gmail.com>
2016-05-01 10:12:22 +1200

    redirect wrong url to front page

--- app.js
+++ app.js
@@ -92,8 +92,11 @@ app.get('/logout', function (req, res) {
 })
 
 app.get('/', function (req, res) {
-  var info= req.session.passport.user
-  var user={id:info.id, name:info.name,email:info.email,image:info.picture.data.url}
+  var user
+  if(req.session.passport){
+    var info= req.session.passport.user
+    user={id:info.id, name:info.name,email:info.email,image:info.picture.data.url}
+  }
   user? res.render('layout',{user}) : res.render('layout')
 })
 
@@ -101,7 +104,7 @@ app.get('/auth/facebook',
    passport.authenticate('facebook', { scope: ['email'] }));
 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  passport.authenticate('facebook', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/')
@@ -109,35 +112,42 @@ app.get('/auth/facebook/callback',
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
+
+app.use(function(req,res){
+
+
+    res.redirect('/')
+
+})
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 
 module.exports = app;
