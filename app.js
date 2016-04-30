fe5447b1f0438ce4f23de548078ae096c408f1db
Vicken <vicken.liu@gmail.com>
2016-04-30 23:12:43 +1200

    missing ; in css

--- app.js
+++ app.js
@@ -121,22 +121,24 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.redirect('/')
+    // res.status(err.status || 500);
+    // res.render('error', {
+    //   message: err.message,
+    //   error: err
+    // });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.redirect('/')
+  // res.status(err.status || 500);
+  // res.render('error', {
+  //   message: err.message,
+  //   error: {}
+  // });
 });
 
 
