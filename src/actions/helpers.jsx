953ba5c57bf0b019fee70f2559f6e432b9dcbf57
Vicken <vicken.liu@gmail.com>
2018-04-09 08:56:19 +1200

    remove url

--- src/actions/helpers.jsx
+++ src/actions/helpers.jsx
@@ -22,7 +22,7 @@ export function deleteData(url){
 
 export function fetchInitData(store){
   return new Promise((resolve,reject) => {
-    request.get('http://localhost:3000/init')
+    request.get('/init')
            .end(function(err,data){
              console.log('here is data',err)
              if(err){
